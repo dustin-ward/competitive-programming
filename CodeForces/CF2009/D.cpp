be796fbe5375c5b102923e418fc0e6cce2d3ca86
Dustin Ward <wardddustin@gmail.com>
2024-09-16 15:54:40 -0400

    CF2009

---
+++ CodeForces/CF2009/D.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+typedef vector<int> vi;
+
+#define INF 1000000000
+#define fst first
+#define snd second
+#define debug(a) cerr << #a << " = " << (a) << endl;
+#define sz(x) (int)(x).size()
+#define all(X) begin(X), end(X)
+#define rall(X) rbegin(X), rend(X)
+
+const double PI = acos(-1);
+double time() { return double(clock()) / CLOCKS_PER_SEC; }
+
+template<typename T, typename U> ostream& operator<<(ostream& o, const pair<T, U>& x) { o << "(" << x.fst << ", " << x.snd << ")"; return o; }
+template<typename T> ostream& operator<<(ostream& o, const vector<T>& x) { o << "["; int b = 0; for (auto& a : x) o << (b++ ? ", " : "") << a; o << "]"; return o; }
+template<typename T> ostream& operator<<(ostream& o, const set<T>& x) { o << "{"; int b = 0; for (auto& a : x) o << (b++ ? ", " : "") << a; o << "}"; return o; }
+template<typename T> ostream& operator<<(ostream& o, const multiset<T>& x) { o << "{"; int b = 0; for (auto& a : x) o << (b++ ? ", " : "") << a; o << "}"; return o; }
+template<typename T> ostream& operator<<(ostream& o, const unordered_set<T>& x) { o << "{"; int b = 0; for (auto& a : x) o << (b++ ? ", " : "") << a; o << "}"; return o; }
+template<typename T> ostream& operator<<(ostream& o, const unordered_multiset<T>& x) { o << "{"; int b = 0; for (auto& a : x) o << (b++ ? ", " : "") << a; o << "}"; return o; }
+template<typename T, typename U> ostream& operator<<(ostream& o, const map<T, U>& x) { o << "{"; int b = 0; for (auto& a : x) o << (b++ ? ", " : "") << a; o << "}"; return o; }
+template<typename T, typename U> ostream& operator<<(ostream& o, const multimap<T, U>& x) { o << "{"; int b = 0; for (auto& a : x) o << (b++ ? ", " : "") << a; o << "}"; return o; }
+template<typename T, typename U> ostream& operator<<(ostream& o, const unordered_map<T, U>& x) { o << "{"; int b = 0; for (auto& a : x) o << (b++ ? ", " : "") << a; o << "}"; return o; }
+template<typename T, typename U> ostream& operator<<(ostream& o, const unordered_multimap<T, U>& x) { o << "{"; int b = 0; for (auto& a : x) o << (b++ ? ", " : "") << a; o << "}"; return o; }
+
+int main() {
+    int T; cin>>T;
+    while(T--) {
+        int N; cin>>N;
+        set<pii> points;
+        unordered_map<int,int> m;
+        for(int i=0; i<N; i++) {
+            int x,y;
+            cin>>x>>y;
+            points.emplace(x,y);
+            m[x]++;
+        }
+
+        ll ans = 0;
+
+        // Up-Down right angle
+        for(auto &[_,c] : m) {
+            if(c==2) ans += N-2;
+        }
+
+        // Turned
+        for(auto &point : points) {
+            pii p2 = {point.fst-1, (point.snd+1)%2};
+            pii p3 = {point.fst+1, (point.snd+1)%2};
+
+            if(points.count(p2) && points.count(p3)) ans++;
+        }
+
+        cout<<ans<<endl;
+    }
+}
